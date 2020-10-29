96e16cac38de20a9ab5a2058c78d1fd01447aa31
João Vasconcelos <jvsn19@gmail.com>
2020-10-28 23:02:47 -0300

    adding error handler to GET /balance
    If no id is provided, return a 404

--- ebanx-api-backend/routes/balance.py
+++ ebanx-api-backend/routes/balance.py
@@ -1,5 +1,11 @@
+from flask import request
 from . import routes
 
-@routes.route('/balance')
+@routes.route('/balance', methods=['GET'])
 def balance():
-    return 'Balance'
+    account_id = request.args.get('account_id')
+
+    if account_id is None:
+        return ('0', 404)
+
+    return f'Balance {account_id}'
