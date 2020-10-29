565b3818b6db1f58fcf8dbb8b8a75ccfb5965a26
João Vasconcelos <jvsn19@gmail.com>
2020-10-29 18:59:00 -0300

    updating mydb.py
    mydb now can handle any requests from routes

--- ebanx-api-backend-flask/utils/mydb.py
+++ ebanx-api-backend-flask/utils/mydb.py
@@ -1,12 +1,13 @@
 from ..models import Account
 from .event_types import EventType
+from .exceptions import NonExistingAccountException
 
 class SingletonDB(type):
     _instance = None
 
-    def __call__(cls, *args, **kwargs):
+    def __call__(cls):
         if cls._instance is None:
-            cls._instance = super().__call__(*args, **kwargs)
+            cls._instance = super().__call__()
         return _instance
 
 
@@ -19,31 +20,96 @@ class DB(metaclass = SingletonDB):
     '''
     _accounts = {}
 
-    # Private Methods
-    def _withdraw(self, **kwargs):
-        pass
+    @classmethod
+    def _withdraw(cls, **kwargs):
+        '''
+        Withdraw an amount in a given account.
+        If the account doesn't exist, throws an
+        NonExistingAccountException error
+        '''
+        origin = kwargs.get('origin')
+        amount = int(kwargs.get('amount'))
 
-    def _deposit(self, **kwargs):
-        pass
+        if not cls.has_account(origin):
+            raise NonExistingAccountException(origin)
+
+        account = cls._get_account(origin)
+        account.balance = account.balance - amount
+
+        return account
+
+    @classmethod
+    def _deposit(cls, **kwargs):
+        '''
+        Deposits an amount in a given account.
+        If account doesn't exists, create one with amount as balance
+        '''
+        destination = kwargs.get('destination')
+        amount = int(kwargs.get('amount'))
+
+        if not cls.has_account(destination):
+            cls._create_account(destination)
+
+        account = cls._get_account(destination)
+        account.balance = account.balance + amount
+
+        return account
 
     @classmethod
     def _get_account(cls, account_id):
         '''
-        Tries to get a valid account. If there is no account, create one
-        with amount = 0
+        Tries to get a valid account.
         '''
-        if account_id not in cls._accounts:
-            cls._accounts[account_id] = Account(account_id)
-        return cls._accounts[account_id]
-
-     # Public Methods
-    @classmethod
-    def event(cls, event_type, **kwargs):
-        pass
+        return cls._accounts.get(account_id)
 
     @classmethod
-    def balance(cls, **kwargs):
-        pass
+    def _create_account(cls, account_id, amount = 0):
+        '''
+        Creates a new account with amount as balance.
+        Default amount is 0
+        '''
+        cls._accounts[account_id] = Account(account_id, amount)
+
+    @classmethod
+    def has_account(cls, account_id):
+        '''
+        Check if account_id is associated with an account
+        '''
+        return cls._get_account(account_id) is not None
+
+    @classmethod
+    def event(cls, **kwargs):
+        '''
+        event handler method. This method handles:
+        - withdrawn: remove an amount from an account
+        - deposit: add an amount to an account
+        - transfer: transfer an amount between origin and destiny
+        '''
+        event_type = kwargs.get('type')
+        if event_type == 'deposit':
+            changed_destination = cls._deposit(**kwargs)
+            return changed_destination.to_dict()
+
+        elif event_type == 'transfer':
+            changed_origin = cls._withdraw(**kwargs)
+            changed_destination = cls._deposit(**kwargs)
+
+            return {
+                'origin': changed_origin.to_dict(),
+                'destination': changed_destination.to_dict()
+            }
+
+        elif event_type == 'withdraw':
+            changed_origin = cls._withdraw(**kwargs)
+            return changed_origin.to_dict()
+
+
+    @classmethod
+    def balance(cls, account_id):
+        '''
+        Method to return the balance of a given account
+        '''
+        return cls._get_account(account_id).balance
 
     @classmethod
     def reset(cls):
