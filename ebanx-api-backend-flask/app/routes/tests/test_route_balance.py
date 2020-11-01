f441d8d2e177d4c6813e9168a875666e065c63c3
João Vasconcelos <jvsn19@gmail.com>
2020-11-01 20:15:06 -0300

    fix test_route_balance
    adding db.reset() before each test

--- ebanx-api-backend-flask/app/routes/tests/test_route_balance.py
+++ ebanx-api-backend-flask/app/routes/tests/test_route_balance.py
@@ -1,10 +1,12 @@
 from flask import json
 
-def test_balance_empty_db(app):
+def test_balance_empty_db(app, db):
+    db.reset()
     response = app.get('/balance?account_id=0')
     assert response.status_code == 404
 
 def test_balance_valid_id(app, db):
+    db.reset()
     id = db.create_account(1000)
     response = app.get(f'/balance?account_id={id}')
     assert response.status_code == 200
