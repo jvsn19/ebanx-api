0213fd569f3ff9240d198033c3c45eff1e79b9cb
João Vasconcelos <jvsn19@gmail.com>
2020-11-01 20:05:15 -0300

    update conftest database handler
    - create a class with useful methods to mock database
    - update test_route_balance to use new database handler

--- ebanx-api-backend-flask/conftest.py
+++ ebanx-api-backend-flask/conftest.py
@@ -4,17 +4,25 @@ from app import create_app
 from app.db import CustomDatabase
 from app.models import Account
 
-def mock_database():
-    # Mock database with previous values
-    CustomDatabase._accounts['123'] = Account(123, 200)
-    CustomDatabase._accounts['456'] = Account(456, 100)
-
 @pytest.fixture
 def app():
     return create_app().test_client()
 
 @pytest.fixture
 def db():
-    mock_database()
+    class DatabaseHandler:
+        def __init__(self):
+            self.database = CustomDatabase
+            self.id = 0
 
-    return CustomDatabase
+        def create_account(self, balance = 1000):
+            last_id = self.id
+            self.id += 1
+            CustomDatabase._create_account(str(last_id), balance)
+
+            return last_id
+
+        def reset(self):
+            self.database.reset()
+
+    return DatabaseHandler()
--- ebanx-api-backend-flask/tests/test_route_balance.py
+++ ebanx-api-backend-flask/tests/test_route_balance.py
@@ -1,13 +1,14 @@
 from flask import json
 
-def test_balance_empty_db(app, db):
+def test_balance_empty_db(app):
     response = app.get('/balance?account_id=0')
     assert response.status_code == 404
 
 def test_balance_valid_id(app, db):
-    response = app.get('/balance?account_id=123')
+    id = db.create_account(1000)
+    response = app.get(f'/balance?account_id={id}')
     assert response.status_code == 200
 
     data = json.loads(response.get_data(as_text=True))
 
-    assert data == 200
+    assert data == 1000
